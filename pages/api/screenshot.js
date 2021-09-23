7f72793937a63345030f76925c238339cfac48a4
yonghzhang <yonghzhang@tencent.com>
2021-09-23 10:33:59 +0800

    fix:去掉oss上传

--- pages/api/screenshot.js
+++ pages/api/screenshot.js
@@ -1,16 +1,13 @@
 const puppeteer = require('puppeteer');
 const { getQuery } = require('./utils/index.js');
-const { uploadFile } = require('../../utils/ali-oss.js');
 
 module.exports = async (req, res) => {
   const url = getQuery('url', req);
 
   const browser = await puppeteer.launch();
   const [page] = await browser.pages();
-  await page.goto(url || 'https://example.com');
-  const x = await page.screenshot({ path: 'example.png' });
+  await page.goto(url || 'https://www.example.com');
+  const x = await page.screenshot();
   await browser.close();
-  const reseult = await uploadFile(undefined, x);
-
-  res.json(reseult);
+  res.json(x.toString('base64'));
 };
--- pages/screenshot.jsx
+++ pages/screenshot.jsx
@@ -1,19 +1,35 @@
 import { useState } from "react";
+import axios from 'axios';
 import Image from 'next/image';
 
+const url_reg = /http(s)?:\/\/([\w-]+\.)+[\w-]+(\/[\w- .\/?%&=]*)?/;
+
 export default function Screenshot() {
+  const [value, setValue] = useState('');
   const [url, setUrl] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const onChange = e => {
+    const val = e.target.value;
+    setValue(val);
+  }
 
   const onClick = () => {
-    fetch('/api/screenshot')
-      .then(response => response.json())
-      .then(json => json?.url)
-      .then(url => setUrl(url));
+    if (!url_reg.test(value)) return alert('链接格式不正确');
+    setLoading(true);
+    const params = { url: value };
+    axios.get('/api/screenshot', { params })
+      .then(res => res.data)
+      .then(text => `data:image/jpg;base64,${text}`)
+      .then(url => setUrl(url))
+      .finally(() => setLoading(false));
   };
 
   return (
     <>
+      <input value={value} onChange={onChange} placeholder="https://www.example.com" />
       <button onClick={onClick}>截图</button>
+      {loading && '截图中...'}
       {url && <Image src={url} alt="测试图" width={200} height={150} />}
     </>
   );
